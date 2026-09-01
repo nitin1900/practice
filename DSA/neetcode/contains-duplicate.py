08ff2a7298f3dd3d41cd83cf53b644d68b22965b
nitin1900 <nitin1900@tutamail.com>
2026-09-01 14:34:13 +0530

    DSA done more coming today...

---
+++ DSA/neetcode/contains-duplicate.py
@@ -0,0 +1,14 @@
+#done myself but with some error debugged by ai:
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        for i,num in enumerate(nums):
+            for j,numu in enumerate(nums):
+                if num==numu and i!=j:
+                    return True
+        return False #here i have written as else:return false which was wrong like this is correct
+
+#suggusted by ai
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
+
